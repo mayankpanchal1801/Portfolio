f680ffa76171504c95d6a628bb87e22c53f95f26
Mayank Panchal <mayank@screetract.com>
2023-11-17 01:20:41 +0530

    copyright section added to the website

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    const dat = new Date();
+    const yr = dat.getFullYear();
+    return (
+        <div className="footer-bottom">
+            <div className="container m-auto pb-4 text-gray-700">
+                <div className="copyright">
+                    <p className="text-center">&copy; {yr} Mayank Panchal. All Rights Reserved.</p>
+                </div>
+                <div className="credits">
+                    <p className="text-center">
+                        Designed and built with ❤️ by{" "}
+                        <a href="https://www.yourportfolio.com" target="_blank" rel="noopener noreferrer">
+                            Mayank
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
